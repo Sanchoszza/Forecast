bf8053ab1e4ebd6676dc255dc9f6fcd23029b5a1
Sanchosszza <87428097+Sanchoszza@users.noreply.github.com>
2024-05-30 13:08:58 +0300

    Add files via upload

---
+++ WeatherWidget/WeatherWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherWidgetBundle.swift
+//  WeatherWidget
+//
+//  Created by Alexandra on 29.05.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WeatherWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WeatherWidget()
+    }
+}
